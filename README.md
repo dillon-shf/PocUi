5edf8a225ebdb57ad1f7afb71dc90556b7f39853
dillonshf <dillon.cordova@superhifi.com>
2021-03-05 11:20:14 -0800

    asd

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Ng8aws
+# poc_ui
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.24.
 
--- app.component.spec.ts
+++ app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng8aws'`, () => {
+  it(`should have as title 'poc_ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ng8aws');
+    expect(app.title).toEqual('poc_ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ng8aws app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('poc_ui app is running!');
   });
 });
