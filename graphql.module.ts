688ff156f16c34ce2e03be93f5ef8b5410297388
dillonshf <dillon.cordova@superhifi.com>
2021-03-05 10:56:26 -0800

    asd

--- graphql.module.ts
+++ graphql.module.ts
@@ -1,12 +1,27 @@
 import {NgModule} from '@angular/core';
 import {APOLLO_OPTIONS} from 'apollo-angular';
-import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
+import {ApolloClientOptions, InMemoryCache, ApolloLink} from '@apollo/client/core';
 import {HttpLink} from 'apollo-angular/http';
+import { setContext } from '@apollo/client/link/context';
 
-const uri = 'http://localhost:3000/graphql'; // <-- add the URL of the GraphQL server here
+const uri = 'https://admin.dev.superhi.fi/graphql'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+  const basic = setContext( () => ({
+    headers: {
+      Accept: 'charset=utf-8'
+    }
+  }));
+
+  const auth = setContext( () => ({
+    headers: {
+      "Authorization": "eyJraWQiOiIwbjdlRTNlZkpYejErUUxWTHlHWFRGXC84ZWg3eDgwdEVkWFJTTCtiRDA4QT0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI0MjEyZGM5Ny0yZThkLTQ1ODAtOGVhNC1kMzQzNjVhOGVjNjEiLCJhdWQiOiI2cWU3M3JzNG41c2pyazMyMjc3a2o0aWNwaSIsImNvZ25pdG86Z3JvdXBzIjpbIlFBIl0sImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJldmVudF9pZCI6IjZmYmUxMjQxLTEyNjYtNGYwZC05YTZmLTcwY2QzZTgwMTk2YiIsInRva2VuX3VzZSI6ImlkIiwiYXV0aF90aW1lIjoxNTkxNDgxNjgyLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtd2VzdC0yLmFtYXpvbmF3cy5jb21cL3VzLXdlc3QtMl9mT290WjYzT00iLCJjb2duaXRvOnVzZXJuYW1lIjoiY2hhZC5idW1zdGVhZEBzdXBlcmhpZmkuY29tIiwiZXhwIjoxNTkyMzQ4ODA3LCJpYXQiOjE1OTIzNDUyMDcsImVtYWlsIjoiY2hhZC5idW1zdGVhZEBzdXBlcmhpZmkuY29tIn0.N-I8WJnHDUrvNQq6Ga-065XvhzdCgHMC2zsEyfejinUt17iPXa37tPW2a2DHTpkRW9CtzYUe8pJjO6OSW9fGfEYa3FiCnGRpzEM_f_TjV_UbMRn50IhyRd2DSZ5JxPbttUE-asWdB0s40cXYY0C-fr0bEpDjAvKQPUyZX-uuO13dp1HkIIaiiEuMEW9JprcJUzEiuMn_J2MSbDBcWf2KUDlieCxO3JLMxLwE8L5Dhy83zH9m3XTtnWtvHCpY6ThdnDpBuvP47VKL201b-gS1CELno2cZ51lyeyTFs_aZBjhlDe_cnmqG0XO012CP1RWIW-oIxILwMUgzbKW_v0QE4Q"
+    }
+  }));
+
+  const link = ApolloLink.from([basic, auth, httpLink.create({ uri })]);
+
   return {
-    link: httpLink.create({uri}),
+    link: link, //httpLink.create({uri}),
     cache: new InMemoryCache(),
     defaultOptions: {
       query: {
--- services/upload.service.ts
+++ services/upload.service.ts
@@ -9,11 +9,12 @@ export class UploadService {
   constructor() { }
 
   uploadFile(file) {
+    debugger;
     const bucket = new S3({
       region: 'us-west-2',
-      accessKeyId: 'asd',
-      secretAccessKey: 'asd/U+Oe9hdD/p4',
-      sessionToken: 'asd'
+      accessKeyId: 'ASIAZGUI3PT5KEITEZVP',
+      secretAccessKey: 'EWX/wWk7I1b+b5q0fcTuSzSudDlxkicjmXxvCNY4',
+      sessionToken: 'IQoJb3JpZ2luX2VjENv//////////wEaCXVzLXdlc3QtMiJHMEUCIQD4zOMJPU8nwaHkaJt88jLAafOqKkOkG1fso1EXoklweQIgFkUC77MxC/QSsIanXBpkpGYZoSzO2vDJ4F//i09EHNYqgwMI9P//////////ARABGgw2MzI3MjA5NDg0NzQiDAECvh/MjFg1ha7/qyrXAr+rRac4coacrzycH7zDc8hE++4i1JumuXoGAxHmNg0EMzAkEa9yETBy1Fg/HN3ajJxQZBBpAt5pyDA7VrpTjqd1Efb4BbMZQsR4DJoK3V+3aOHMD5GfifcqMh8xBJwM3eTVnKQEhlBmTMjXHHcWio2zkRRtLLBkDtbVFrCPOyuPzuVfiyL3KEMtleUh5LuTBf7hrnxI1o6ptFj9FFF7edmMFD40oZcZEBiI6ELk5kD39hPR6Un6SSNuiQHjpWiRIOXke2rBjTKoJ5skFqAAq8+cQSBkmOdWgzCEFhOwFn0WJGz2fMimCa7dIcj5IIku1j4mKt++jCKc58RmFvX6aWeapjNFcNBQpvr/xqTRfO+IWKdySm2Zq/kSTwGYcc94+ut3xJ12N6R1Af3zX3zeed4wE5mXM/CCwKUTlfVe7Bn/iSmwDSw2eFTg/ybzlQ5wKd7UG8HQvQ8wy/qJggY6pgG2OKHtbNv3ucX5E/ZBispY42SShABWn9lGA+XjddvuwvaW5NGu+uCxTXJNo6P+NUtuqItDWcCvPiGCkIWK+b8O+S7PplBpc3QNKYLd2bmcJoY9MDduDKYK3668j2C4P2hMEJ6fc0Efo9437J+fUOeNqGZjxEjRGxkKjnMAo7ApEii0tYyIKq3NDN7rltTvkaKeHsQf2DpQAulGxyd6FGW3AE7Hy3xu'
     });
 
     const params = {
